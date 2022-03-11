49941bba71eb33133355472725a0bf382332f7bc
Sedonn <maxsokolov137@gmail.com>
2022-03-11 14:46:29 +0300

    Created download file function

--- Controllers/file.controllers.ts
+++ Controllers/file.controllers.ts
@@ -6,20 +6,38 @@ import { Request, Response, NextFunction } from 'express';
 export module FileControllers {
     export const uploadFile = async (req: Request, res: Response) => {
         if (req.file) {
-            const bucket = await new mongoose.mongo.GridFSBucket(mongoose.connection.db);
+            const bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
 
-            streamifier.createReadStream(req.file.buffer)
-                .pipe(bucket.openUploadStream(req.file.filename, {
-                    metadata: { status: 'awaiting' },
-                }));
+            streamifier.createReadStream(req.file.buffer).pipe(
+                bucket.openUploadStream(req.file.originalname, {
+                    metadata: { mimetype: req.file.mimetype },
+                })
+            );
 
             res.json({ status: 'uploaded' });
         } else {
-            res.json({ error: 'file upload error' });
+            res.status(400).json({ error: 'file upload error' });
         }
     };
 
-    export const downloadFile = (req: Request, res: Response, next: NextFunction) => {
+    export const downloadFile = async (req: Request, res: Response, next: NextFunction) => {
+        if (req.query['file-name']) {
+            const db = mongoose.connection.db;
+            const bucket = new mongoose.mongo.GridFSBucket(db);
 
+            const fileCollection = db.collection('fs.files');
+            const file = await fileCollection.findOne({ filename: req.query['file-name'] });
+
+            if (file) {
+                res.setHeader('Content-disposition', `attachment; filename=${file.filename}`);
+                res.setHeader('Content-type', file.metadata.mimetype);
+
+                bucket.openDownloadStream(file._id).pipe(res);
+            } else {
+                res.status(404).json({ error: 'file not found' });
+            }
+        } else {
+            res.status(400).json({ error: 'bad request' });
+        }
     };
 }
