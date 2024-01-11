87eecb91fa17df74fd99701995e354af8a8754de
Sedonn <maxsokolov137@gmail.com>
2024-01-11 16:55:23 +0300

    Add docker development configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import ViteFonts from 'unplugin-fonts/vite';
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,6 +39,10 @@ export default defineConfig({
     extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: process.env.VITE_PORT,
+    watch: {
+      usePolling: true,
+    },
   },
-})
+});
--- start-dev.bat
+++
@@ -1,3 +0,0 @@
-start cmd.exe /k "cd ./api/ & npm run dev"
-
-start cmd.exe /k "cd ./client/ & npm run dev"
